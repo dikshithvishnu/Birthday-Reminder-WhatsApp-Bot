841310d0f2f245f348c16ca5983d31471fb8ae62
Dikshith <60872546+dikshithvishnu@users.noreply.github.com>
2021-07-19 13:23:10 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,24 +1,25 @@
+# Importing required packages
 import pandas as pd
 from datetime import date
 from sqlalchemy import create_engine
 import pywhatkit as kit
 
+# Storing today's date
 today = date.today()
+# Changing today's date to 'dd-mm' format
 today_format = today.strftime('%d-%m')
 
+# Establishing a connection to AWS RDS MySQL instance
 connection = create_engine('mysql+pymysql://admin:shitindianssay@database-1.cke6baraqvyb.ap-south-1.rds.amazonaws.com:3306/memechat')
 df = pd.read_sql('SELECT * FROM `employee_details` ORDER BY EXTRACT(MONTH FROM DOB), EXTRACT(DAY FROM DOB) ASC', connection)
+# Storing the DOB column in 'dd-mm' format
 df['DOB'] = pd.to_datetime(df['DOB']).dt.strftime('%d-%m')
-# print(df)
 
 def check_bday():
     for index, row in df.iterrows():
         try:
             if df['DOB'][index] == today_format:
                 kit.sendwhatmsg("+91{9740979450}", "Today is " + df['Name'][index] + "'s birthday", 11, 24)
-                #kit.sendwhatmsg_to_group("Chess", "Today is " + df['Name'][index] + "'s birthday", 1, 11)
-                #kit.image_to_ascii_art("C:/Users/user/Downloads/wie05t4a9mb71.jpg", "C:/Users/user/Downloads/wie05t4a9mb71")
-                # kit.playonyt("Yah Mean")
         except Exception as e:
             pass
 
